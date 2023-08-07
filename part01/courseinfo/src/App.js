b857cf4f2cd7983e0de33c156b5d40437b96d9bf
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-08-07 17:14:03 +0100

    second part completed

--- part01/courseinfo/src/App.js
+++ part01/courseinfo/src/App.js
@@ -2,11 +2,28 @@ const Header = ({ course }) => {
   return <h1>{course}</h1>
 }
 
-const Content = ({ part, exercises }) => {
+const Part = ({ part, exercises }) => {
   return (
     <p>
       {part} {exercises}
     </p>
+  )
+}
+
+const Content = ({
+  part1,
+  exercises1,
+  part2,
+  exercises2,
+  part3,
+  exercises3,
+}) => {
+  return (
+    <>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+    </>
   )
 }
 
@@ -17,7 +34,7 @@ const Total = ({ exercises }) => {
 const App = () => {
   const course = "Half Stack application development"
   const part1 = "Fundamentals of React"
-  const exercise1 = 10
+  const exercises1 = 10
   const part2 = "Using props to pass data"
   const exercises2 = 7
   const part3 = "State of a component"
@@ -26,11 +43,15 @@ const App = () => {
   return (
     <>
       <Header course={course} />
-      <Content part={part1} exercise={exercise1} />
-      <Content part={part2} exercise={exercises2} />
-      <Content part={part3} exercise={exercises3} />
-
-      <Total exercises={exercise1 + exercises2 + exercises3} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+      <Total exercises={exercises1 + exercises2 + exercises3} />
     </>
   )
 }
