2f6bbed5206e59f316dccb1ed485a969c5b28c1d
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-08-08 10:01:36 +0100

    refactored by simplifying code

--- part01/courseinfo/src/App.js
+++ part01/courseinfo/src/App.js
@@ -10,19 +10,12 @@ const Part = ({ part, exercises }) => {
   )
 }
 
-const Content = ({
-  part1,
-  exercises1,
-  part2,
-  exercises2,
-  part3,
-  exercises3,
-}) => {
+const Content = ({ parts }) => {
   return (
     <div>
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
+      <Part part={parts[0].name} exercises={parts[0].exercises} />
+      <Part part={parts[1].name} exercises={parts[1].exercises} />
+      <Part part={parts[2].name} exercises={parts[2].exercises} />
     </div>
   )
 }
@@ -51,14 +44,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={parts[0].name}
-        exercises1={parts[0].exercises}
-        part2={parts[1].name}
-        exercises2={parts[1].exercises}
-        part3={parts[2].name}
-        exercises3={parts[2].exercises}
-      />
+      <Content parts={parts} />
       <Total
         exercises={parts[0].exercises + parts[1].exercises + parts[2].exercises}
       />
