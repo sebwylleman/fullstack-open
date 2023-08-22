f28e891a5716a7d3a5f6ca0d2fa78fe35616e442
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-08-22 09:34:33 +0100

    refactored to use an object as feedback

--- part01/unicafe/src/App.jsx
+++ part01/unicafe/src/App.jsx
@@ -6,42 +6,37 @@ const Button = ({ handleClick, label }) => (
   <button onClick={handleClick}>{label}</button>
 )
 
-const Feedback = (props) => {
+const Feedback = ({ feedbackScore }) => {
   return (
     <div>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
+      <p>good {feedbackScore.good}</p>
+      <p>neutral {feedbackScore.neutral}</p>
+      <p>bad {feedbackScore.bad}</p>
     </div>
   )
 }
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
 
-  const handleGoodClick = () => {
-    setGood(good + 1)
-  }
+  const [feedbackScore, setfeedbackScore] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  })
 
-  const handleNeutralClick = () => {
-    setNeutral(neutral + 1)
-  }
-
-  const handleBadClick = () => {
-    setBad(bad + 1)
+  const handleClick = (type) => {
+    setfeedbackScore({ ...feedbackScore, [type]: feedbackScore[type] + 1 })
   }
 
   return (
     <div>
       <Title text="give feedback" />
-      <Button handleClick={handleGoodClick} label="good" />
-      <Button handleClick={handleNeutralClick} label="neutral" />
-      <Button handleClick={handleBadClick} label="bad" />
+      <Button handleClick={() => handleClick("good")} label="good" />
+      <Button handleClick={() => handleClick("neutral")} label="neutral" />
+      <Button handleClick={() => handleClick("bad")} label="bad" />
       <Title text="statistics" />
-      <Feedback good={good} neutral={neutral} bad={bad} />
+      <Feedback feedbackScore={feedbackScore} />
     </div>
   )
 }
