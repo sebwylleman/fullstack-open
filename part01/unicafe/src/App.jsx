890c74784796d5872896a56681e3213f10859a9e
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-08-22 08:23:13 +0100

    added event handler + display of each click

--- part01/unicafe/src/App.jsx
+++ part01/unicafe/src/App.jsx
@@ -6,20 +6,42 @@ const Button = ({ handleClick, label }) => (
   <button onClick={handleClick}>{label}</button>
 )
 
+const Feedback = (props) => {
+  return (
+    <div>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const handleGoodClick = () => {}
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
 
   return (
     <div>
       <Title text="give feedback" />
       <Button handleClick={handleGoodClick} label="good" />
-      <Button handleClick={handleGoodClick} label="neutral" />
-      <Button handleClick={handleGoodClick} label="bad" />
+      <Button handleClick={handleNeutralClick} label="neutral" />
+      <Button handleClick={handleBadClick} label="bad" />
       <Title text="statistics" />
+      <Feedback good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
