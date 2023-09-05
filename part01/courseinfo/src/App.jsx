303d52f4b7e3fbbd1460b866d645606ad6e58021
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-09-05 13:20:06 +0100

    refactored by adding a new Part component

--- part01/courseinfo/src/App.jsx
+++ part01/courseinfo/src/App.jsx
@@ -1,20 +1,24 @@
 const Header = (props) => <h1>{props.course}</h1>
 
+const Part = (props) => (
+  <p>
+    {props.part} {props.exercises}
+  </p>
+)
+
 const Content = (props) => (
   <div>
-    <p>
-      {props.part1} {props.exercises1}
-    </p>
-    <p>
-      {props.part2} {props.exercises2}
-    </p>
-    <p>
-      {props.part3} {props.exercises3}
-    </p>
+    <Part part={props.part1} exercises={props.exercises1} />
+    <Part part={props.part2} exercises={props.exercises2} />
+    <Part part={props.part3} exercises={props.exercises3} />
   </div>
 )
 
-const Total = (props) => (<p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>)
+const Total = (props) => (
+  <p>
+    Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+  </p>
+)
 
 const App = () => {
   const course = "Half Stack application development"
@@ -36,9 +40,12 @@ const App = () => {
         exercises2={exercises2}
         exercises3={exercises3}
       />
-      <Total e exercises1={exercises1}
+      <Total
+        e
+        exercises1={exercises1}
         exercises2={exercises2}
-        exercises3={exercises3}/>
+        exercises3={exercises3}
+      />
     </>
   )
 }
