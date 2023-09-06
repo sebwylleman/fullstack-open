222a34b637774b718aeb92d848662f5a439b3d6f
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-09-06 12:19:46 +0100

    step1 completed

--- part01/unicafe/src/App.jsx
+++ part01/unicafe/src/App.jsx
@@ -1,12 +1,32 @@
 import { useState } from "react"
 
+const Heading = ({ text }) => <h1>{text}</h1>
+
+const Button = ({ vote, label }) => <button onClick={vote}>{label}</button>
+
+const Stats = ({ label, feedback }) => (
+  <p>
+    {label} {feedback}
+  </p>
+)
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  return <div>code here</div>
+  return (
+    <div>
+      <Heading text="give feedback" />
+      <Button vote={() => setGood(good + 1)} label="good" />
+      <Button vote={() => setNeutral(neutral + 1)} label="neutral" />
+      <Button vote={() => setBad(bad + 1)} label="bad" />
+      <Heading text="statistics" />
+      <Stats label={"good"} feedback={good} />
+      <Stats label={"neutral"} feedback={neutral} />
+      <Stats label={"bad"} feedback={bad} />
+    </div>
+  )
 }
 
 export default App
