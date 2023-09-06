49a28d6540376b272f253e476b3a788fcc850f35
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-09-06 16:03:44 +0100

    removing comment

--- part01/unicafe/src/App.jsx
+++ part01/unicafe/src/App.jsx
@@ -31,7 +31,6 @@ const Statistics = (props) => {
   )
 }
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
