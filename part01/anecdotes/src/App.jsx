ae5351af69220e58c8aea6a36d874ede4d980f1e
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-11-20 07:19:12 +0000

    formatting change

--- part01/anecdotes/src/App.jsx
+++ part01/anecdotes/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.',
+    'The only way to go fast, is to go well.'
   ]
 
   const [selected, setSelected] = useState(0)
