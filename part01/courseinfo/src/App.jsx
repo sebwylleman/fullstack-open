7a382f2323eb24df4c715e7183a655962c2eb6a0
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-12-12 11:42:34 +0000

    config: eslint

--- part01/courseinfo/src/App.jsx
+++ part01/courseinfo/src/App.jsx
@@ -1,41 +1,47 @@
-const Header = ({title}) => <h1>{title}</h1>
+const Header = ({ title }) => <h1>{title}</h1>
 
-const Part = ({part, exercises}) => <p>{part}: {exercises}</p>
+const Part = ({ part, exercises }) => (
+  <p>
+    {part}: {exercises}
+  </p>
+)
 
-const Content = ({parts}) => {
+const Content = ({ parts }) => {
   return (
     <div>
-      <Part part={parts[0].name} exercises={parts[0].exercises}/>
-      <Part part={parts[1].name} exercises={parts[1].exercises}/>
-      <Part part={parts[2].name} exercises={parts[2].exercises}/>
+      <Part part={parts[0].name} exercises={parts[0].exercises} />
+      <Part part={parts[1].name} exercises={parts[1].exercises} />
+      <Part part={parts[2].name} exercises={parts[2].exercises} />
     </div>
   )
 }
 
-const Total = ({total}) => <p>Total: {total}</p> 
+const Total = ({ total }) => <p>Total: {total}</p>
 
 const App = () => {
-  const course = 'Half Stack application development'
+  const course = "Half Stack application development"
   const parts = [
     {
-      name: 'Fundamentals of React',
-      exercises: 10
+      name: "Fundamentals of React",
+      exercises: 10,
     },
     {
-      name: 'Using props to pass data',
-      exercises: 7
+      name: "Using props to pass data",
+      exercises: 7,
     },
     {
-      name: 'State of a component',
-      exercises: 14
-    }
+      name: "State of a component",
+      exercises: 14,
+    },
   ]
 
   return (
     <div>
-      <Header title={course}/>
-      <Content parts={parts}/>
-      <Total total={parts[0].exercises + parts[1].exercises + parts[2].exercises}/>
+      <Header title={course} />
+      <Content parts={parts} />
+      <Total
+        total={parts[0].exercises + parts[1].exercises + parts[2].exercises}
+      />
     </div>
   )
 }
