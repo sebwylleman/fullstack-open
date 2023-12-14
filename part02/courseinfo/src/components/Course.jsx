2a1bd2a68610710c2471adf9fa74da1e20b135e8
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-12-14 12:16:05 +0000

    refactor: using iteration to display course data instead of printing each element manually

--- part02/courseinfo/src/components/Course.jsx
+++ part02/courseinfo/src/components/Course.jsx
@@ -1,20 +1,31 @@
-const Header = ({ title }) => <h1>{title}</h1>
+const Header = ({ title }) => <h2>{title}</h2>
 
-const Course = ({ course }) => {
-  const totalExercises = course.parts.reduce(
-    (sum, current) => (sum += current.exercises),
-    0
-  )
-  console.log(totalExercises)
+const Content = ({ parts }) => {
   return (
     <div>
-      <Header title={course.name} />
-      {course.parts.map((part) => (
+      {parts.map((part) => (
         <p key={part.id}>
           {part.name} {part.exercises}
         </p>
       ))}
-      <strong>total of {totalExercises} exercises</strong>
+    </div>
+  )
+}
+
+const Total = ({ parts }) => {
+  const totalExercises = parts.reduce(
+    (sum, current) => (sum += current.exercises),
+    0
+  )
+  return <strong>total of {totalExercises} exercises</strong>
+}
+
+const Course = ({ course, name }) => {
+  return (
+    <div>
+      <Header title={name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
