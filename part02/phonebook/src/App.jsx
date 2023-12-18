e9879ff9b322a467b5981e0f675d7a70af3c30f8
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-12-18 12:41:14 +0000

    feature: exercise 2.9

--- part02/phonebook/src/App.jsx
+++ part02/phonebook/src/App.jsx
@@ -2,14 +2,24 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "07826838346" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [stringSearch, setStringSearch] = useState("");
+  const [showAll, setShowAll] = useState(true);
 
   const handleNameChange = (event) => setNewName(event.target.value);
 
   const handleNumChange = (event) => setNewNumber(event.target.value);
+
+  const handleStringSearch = (event) => {
+    setStringSearch(event.target.value);
+    setShowAll(false);
+  };
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -25,11 +35,19 @@ const App = () => {
     setNewName("");
   };
 
-  console.log(persons);
+  const personsToShow = showAll
+    ? persons
+    : persons.filter((person) =>
+        person.name.toLowerCase().includes(stringSearch.toLowerCase())
+      );
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input value={stringSearch} onChange={handleStringSearch} />
+      </div>
       <form onSubmit={addPerson}>
         <div>
           name:
@@ -45,7 +63,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
 
-      {persons.map((person) => (
+      {personsToShow.map((person) => (
         <p key={person.name}>
           {person.name} {person.number}
         </p>
